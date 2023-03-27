a8ec491900e980c89d129d9c3df7426436e3c76b
NikhilLella <nikhileswarlella@gmail.com>
2023-03-27 23:16:54 +0530

    use navigation, outlet, use params

--- src/index.js
+++ src/index.js
@@ -2,19 +2,73 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Link, Navigate, Outlet, Route, Routes, useParams} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <Router>
   <Routes>
     <Route path ="/"  element ={<Home/>}/>
+    <Route path="/learn" element={<Learn/>}>
+      <Route path="courses" element={<Courses/>}>
+        <Route path=":courseId" element={<CourseId/>}/>
+        </Route>
+
+      <Route path="bundles" element={<Bundles/>}/>
+      <Route/>
+    </Route>
+    <Route path ="/my-apps" element ={ <Navigate replace to ="/learn"/>}/>
   </Routes>
  </Router>
 );
 
 function Home(){ //home component
   return (<h1> Home Router</h1>);
+}
+
+function Learn(){
+  return (
+    <div>
+      <h1>Learn</h1>
+      <h4>All courses go here</h4>
+      <Link className='btn btn-success' to ="/learn/courses">
+        courses
+      </Link> {" "}
+<Link  className='btn btn-primary' to ="/learn/bundles"> 
+Bundles
+</Link>   
+<Outlet/>
+ </div>
+  );
+}
+
+function Courses(){
+  return (
+    <div>
+    <h1> Course List</h1>
+    <h4> Course Cards</h4>
+    <Outlet/>
+    </div>
+  );
+}
+
+function Bundles(){
+  return(
+    <div>
+      <h1> Bundle List</h1>
+      <h4> Bundle Cards</h4>
+
+    </div>
+  )
+}
+function CourseId(){
+const {courseId} = useParams();
+  return (
+<div>
+  <h1>The URL parameter is :{courseId}</h1>
+</div>
+
+  )
 }
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
